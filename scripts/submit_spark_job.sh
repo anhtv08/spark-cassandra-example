886cb7a0226fae9556094e5ea189072fd6d0e684
anhtv08 <anhtv010888@gmail.com>
2019-05-31 06:49:36 +0800

    adding spark submit job

--- scripts/submit_spark_job.sh
+++ scripts/submit_spark_job.sh
@@ -5,8 +5,19 @@ WORKING_DIR=$HOME/working/java/sourcecode/spark-cassandra-example
 CONF_FILE=$WORKING_DIR/cassandra-example.conf
 MAIN_JOB_CLASS=org.koeninger.HelloWorldExample
 JAR_FILE=$WORKING_DIR/target/scala-2.10/cassandra-example-assembly-0.1-SNAPSHOT.jar
+APP_NAME="Spark test job"
+
+# copy application jar to spark lib:
+cp $JAR_FILE $SPARK_HOME_DIR/jars/
 
 $SPARK_HOME_DIR/bin/spark-submit \
---properties-file $CONF_FILE
---class $MAIN_JOB_CLASS $JAR_FILE
+--properties-file $CONF_FILE \
+--class $MAIN_JOB_CLASS \
+--name $APP_NAME \
+--jars $JAR_FILE
+
+ret_code=$?
+
+echo "job completed with return code : $ret_code"
+
 
